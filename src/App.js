309fc2a0d18198fb3fdbe78612772781feb40907
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-14 09:17:49 +0300

    form submit func created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
+import { makeStyles } from '@mui/styles';
+import { Box } from '@mui/system';
+import { useState } from 'react';
 import './App.css';
-import ContactList from './components/contactlist/ContactList';
-import FormComp from './components/form/FormComp';
+import ContactList from './components/ContactList';
+import FormComp from './components/FormComp';
+
+const useStyles = makeStyles({
+    App: {
+        width: '100%',
+        height: '100vh',
+        backgroundImage: 'url(https://picsum.photos/1600/900)',
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+});
+
+const initialValues = { name: '', phone: '' };
 
 function App() {
-  return (
-    <div className="App">
-      <FormComp />
-      <ContactList />
-    </div>
-  );
+    const [contact, setContact] = useState(initialValues);
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        console.log(contact);
+        setContact(initialValues);
+    };
+
+    const classes = useStyles();
+    return (
+        <Box className={classes.App}>
+            <FormComp
+                contact={contact}
+                setContact={setContact}
+                handleFormSubmit={handleFormSubmit}
+            />
+            <ContactList />
+        </Box>
+    );
 }
 
 export default App;
