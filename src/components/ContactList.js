792ee48ea17e57503313368c8c7a710545a981b9
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-14 12:12:12 +0300

    contacts fetched from db

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -8,46 +8,66 @@ import {
     Typography,
     ListItemAvatar,
     Paper,
+    CircularProgress,
 } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { Box } from '@mui/system';
 import React from 'react';
+import { useFetchContacts } from '../utils/Firebase';
 
 const useStyles = makeStyles({
-    contactList : {
+    contactList: {
         width: 600,
-        padding: 20
-    }
-})
+        padding: 20,
+    },
+});
 
 const ContactList = () => {
-    const classes = useStyles()
+    const classes = useStyles();
+
+    const { contactList, loading } = useFetchContacts();
+    console.log(loading ? 'loading' : contactList);
+
     return (
         <Paper elevation={12} className={classes.contactList}>
             <Typography sx={{ mt: 4, mb: 2 }} variant="h5" component="div">
                 Contact List
             </Typography>
             <List>
-                <ListItem
-                    secondaryAction={
-                        <Box>
-                            <IconButton edge="end" aria-label="edit">
-                                <Edit />
-                            </IconButton>
-                            <IconButton edge="end" aria-label="delete">
-                                <Delete />
-                            </IconButton>
-                        </Box>
-                    }
-                >
-                    <ListItemAvatar>
-                        <Avatar alt="Person Name" src="#" />
-                    </ListItemAvatar>
-                    <ListItemText
-                        primary="Person Name"
-                        secondary="Phone Number"
-                    />
-                </ListItem>
+                {loading ? (
+                    <CircularProgress />
+                ) : (
+                    contactList.map((contact) => {
+                        return (
+                            <ListItem key={contact.id}
+                                secondaryAction={
+                                    <Box>
+                                        <IconButton
+                                            edge="end"
+                                            aria-label="edit"
+                                        >
+                                            <Edit color="success" />
+                                        </IconButton>
+                                        <IconButton
+                                            edge="end"
+                                            aria-label="delete"
+                                        >
+                                            <Delete color="error" />
+                                        </IconButton>
+                                    </Box>
+                                }
+                            >
+                                <ListItemAvatar>
+                                    <Avatar alt={contact.name} src="#" />
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary={contact.name}
+                                    secondary={contact.phone}
+                                />
+                            </ListItem>
+                        );
+                    })
+                )}
             </List>
         </Paper>
     );
