7c3be3b5142c69c2a9c0a6dde2dfb504d6ac4bd6
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-14 12:12:38 +0300

    fetch contact func written

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -1,21 +1,41 @@
-
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set  } from "firebase/database";
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, push, onValue } from 'firebase/database';
+import { useEffect, useState } from 'react';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBGoVhzwHxbpl10TK-GJUvDTJ-dvwW1aXc",
-  authDomain: "react--contact-list-app.firebaseapp.com",
-  projectId: "react--contact-list-app",
-  storageBucket: "react--contact-list-app.appspot.com",
-  messagingSenderId: "247807283858",
-  appId: "1:247807283858:web:236a1b293a13ab23c78c4b"
+    apiKey: 'AIzaSyBGoVhzwHxbpl10TK-GJUvDTJ-dvwW1aXc',
+    authDomain: 'react--contact-list-app.firebaseapp.com',
+    projectId: 'react--contact-list-app',
+    storageBucket: 'react--contact-list-app.appspot.com',
+    messagingSenderId: '247807283858',
+    appId: '1:247807283858:web:236a1b293a13ab23c78c4b',
 };
-
 
 const app = initializeApp(firebaseConfig);
 
 const database = getDatabase(app);
 
-export const addContact = () => {
-	set(ref(database, "contacts"))
-}
+export const addContact = (contact) => {
+    push(ref(database, 'contacts'), contact);
+};
+
+export const useFetchContacts = () => {
+    const [contactList, setContactList] = useState([]);
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+      setLoading(true)
+        const contactRef = ref(database, 'contacts');
+        onValue(contactRef, (snapshot) => {
+            const data = snapshot.val();
+            const dataArr = [];
+            for (let id in data) {
+                dataArr.push({ id, ...data[id] });
+            }
+            setContactList(dataArr);
+            setLoading(false)
+        });
+    }, []);
+
+    return {contactList, loading};
+};
